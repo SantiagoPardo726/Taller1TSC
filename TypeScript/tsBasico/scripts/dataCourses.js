a2ab9f4b2740338a04fb5b957193ad9e648e3158
SantiagoPardo726 <69635343+SantiagoPardo726@users.noreply.github.com>
2021-10-13 10:42:58 -0500

    Add files via upload

---
+++ TypeScript/tsBasico/scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Sw", "Cesar", 3),
+    new Course("DALGO", "Jorge", 3),
+    new Course("TI", "Julian", 3),
+    new Course("Calculo", "cesar", 3),
+    new Course("Sistrans", "No me acuerdo", 3)
+];
